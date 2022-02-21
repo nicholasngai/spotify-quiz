bff63e0828e704be6d537e3b2fc8e3ff9643f61b
Nicholas Ngai <nicholas@ngai.me>
2022-02-20 22:28:09 -0800

    Add server logger

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
 import path from 'path';
 
 const app = express();
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, 'client')));
 
