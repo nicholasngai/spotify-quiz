8433446b87339d92a907ebf2ccafaedc4dada2ae
Nicholas Ngai <nicholas@ngai.me>
2024-07-13 10:04:43 -0700

    Add example api request

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 
 const app = express();
 app.use(morgan('dev'));
 
 app.use(express.static('www'));
+
+app.get('/api/helloworld', (req: Request, res: Response) => {
+  res.json({
+    data: 'Hello world!',
+  });
+});
 
 export default app;
