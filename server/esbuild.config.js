9bb6f65cc57879e97c356c2115795e1540dcd300
Nicholas Ngai <nicholas@ngai.me>
2024-07-13 10:59:28 -0700

    Build server bundle from tsc output

--- server/esbuild.config.js
+++ server/esbuild.config.js
@@ -3,7 +3,7 @@ const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
 const config = {
   platform: 'node',
-  entryPoints: [path.resolve(__dirname, 'bin/www.ts')],
+  entryPoints: [path.resolve(__dirname, '../build/server/bin/www.js')],
   outfile: path.resolve(__dirname, '../build/dist/server.js'),
   bundle: true,
   minify: true,
