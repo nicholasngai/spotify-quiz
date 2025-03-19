e49a7e508eb997d61f2bc76730eb5d24d95e76b7
Nicholas Ngai <nicholas@ngai.me>
2025-03-19 07:22:12 -0700

    Add GET method to mux

--- internal/controller/server.go
+++ internal/controller/server.go
@@ -12,8 +12,8 @@ func (c Controller) Handler() http.Handler {
 	mux := http.NewServeMux()
 
 	// Routes.
-	mux.Handle("/", http.FileServer(defaultIndexHTMLFileSystem{http.Dir("www")}))
-	mux.Handle("/api/helloworld", http.HandlerFunc(c.HelloWorldHandler))
+	mux.Handle("GET /", http.FileServer(defaultIndexHTMLFileSystem{http.Dir("www")}))
+	mux.Handle("GET /api/helloworld", http.HandlerFunc(c.HelloWorldHandler))
 
 	// Middleware.
 	var handler http.Handler = mux
