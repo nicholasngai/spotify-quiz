be8700fdbb647d3f04e3854d7ca93dec0eba3f2a
Nicholas Ngai <nicholas@ngai.me>
2025-03-18 23:38:23 -0700

    Move file system controller to other file

--- internal/controller/server.go
+++ internal/controller/server.go
@@ -1,32 +1,10 @@
 package controller
 
 import (
-	"errors"
-	"io/fs"
 	"net/http"
 
 	"github.com/nicholasngai/react-template/internal/middlewares"
 )
-
-type defaultIndexHTMLFileSystem struct {
-	fs http.FileSystem
-}
-
-func (fsys defaultIndexHTMLFileSystem) Open(name string) (http.File, error) {
-	// Open file. Return it if it worked.
-	f, err := fsys.fs.Open(name)
-	if err == nil {
-		return f, nil
-	}
-
-	// If not a not-exists error, return the error.
-	if !errors.Is(err, fs.ErrNotExist) {
-		return nil, err
-	}
-
-	// Try to open /index.html instead.
-	return fsys.fs.Open("index.html")
-}
 
 func Handler() http.Handler {
 	mux := http.NewServeMux()
